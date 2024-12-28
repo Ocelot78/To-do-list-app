c869cea3c800fa722d07f4718aa8e0e99e98a85b
Ocelot78 <wojtekkonieczny1@gmail.com>
2024-12-28 15:03:07 +0100

    JSON support (not fully working)

--- Console Version/Todolist.go
+++ Console Version/Todolist.go
@@ -4,6 +4,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"encoding/json"
+	"log"
 )
 type zadanie struct {
 	ID		 int
@@ -42,14 +44,35 @@ func UsunZadanie(IDdoUsun int) {
 	zadania = append(zadania[:IDdoUsun], zadania[IDdoUsun+1:]...)
 	lista_id = append(lista_id[:IDdoUsun], lista_id[IDdoUsun+1:]...)
 }
+func odczytPlik(sciezka string,tablica []zadanie,tablica2 []int) {
+	plik, err := os.ReadFile(sciezka)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var dane []zadanie
+	err = json.Unmarshal(plik, &dane)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tablica = []zadanie{}
+	tablica2 = []int{0}
+	for _,z := range dane {
+		tablica = append(tablica,z)
+		tablica2 = append(tablica2,z.ID)
+	}
+}
+func zapisPlik() {
+
+}
 func main() {
 	var opcja int
 	var UtworzID string
 	var UsunID int
 	var WykonajID int
+	var Sciezka string
 	for {
 		fmt.Print("-----------------------------\n")
-		fmt.Print("Wybierz opcję\n1.Wyświetl zadania.\n2.Dodaj zadanie.\n3.Usuń zadanie.\n4.Oznacz jako wykonane.\n5.Zakończ program.\nNapisz numer opcji:  ")
+		fmt.Print("Wybierz opcję\n1.Wyświetl zadania.\n2.Dodaj zadanie.\n3.Usuń zadanie.\n4.Oznacz jako wykonane.\n5.Wczytaj Zadania z Pliku.\n6.Zapisz zadania do Pliku\n7.Opuść program\nNapisz numer opcji:  ")
 		fmt.Scan(&opcja)
 		switch opcja {
 		case 1:
@@ -75,6 +98,13 @@ func main() {
 			fmt.Scan(&WykonajID)
 			oznacz(WykonajID)
 		case 5:
+			fmt.Print("-----------------------------\n")
+			fmt.Print("Podaj ścieżke do pliku: ")
+			fmt.Scan(&Sciezka)
+			odczytPlik(Sciezka,zadania,lista_id)
+		case 6:
+
+		case 7:
 			os.Exit(0)
 		default:
 			fmt.Print("-----------------------------\n")
