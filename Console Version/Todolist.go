2e7b8328d1215bad94b7ff09acb839352254753c
Ocelot78 <wojtekkonieczny1@gmail.com>
2024-12-29 01:15:48 +0100

    full json file support

--- Console Version/Todolist.go
+++ Console Version/Todolist.go
@@ -1,6 +1,4 @@
 package main
-
-
 import (
 	"fmt"
 	"os"
@@ -12,10 +10,11 @@ type zadanie struct {
 	Nazwa	 string
 	Wykonane bool
 }
+
 var zadania []zadanie
 var lista_id = []int{0}
+var zad zadanie
 func DodajZadanie(Nazwa string) {
-	var zad zadanie
 	for _, v := range lista_id {
 		if v+1 != zad.ID {
 			zad.ID = v+1
@@ -44,7 +43,7 @@ func UsunZadanie(IDdoUsun int) {
 	zadania = append(zadania[:IDdoUsun], zadania[IDdoUsun+1:]...)
 	lista_id = append(lista_id[:IDdoUsun], lista_id[IDdoUsun+1:]...)
 }
-func odczytPlik(sciezka string,tablica []zadanie,tablica2 []int) {
+func odczytPlik(sciezka string) {
 	plik, err := os.ReadFile(sciezka)
 	if err != nil {
 		log.Fatal(err)
@@ -54,15 +53,23 @@ func odczytPlik(sciezka string,tablica []zadanie,tablica2 []int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	tablica = []zadanie{}
-	tablica2 = []int{0}
-	for _,z := range dane {
-		tablica = append(tablica,z)
-		tablica2 = append(tablica2,z.ID)
+	*&zadania = dane
+	*&lista_id = make([]int, len(dane))
+	lista_id = append(lista_id, 0)
+	for i,z := range dane {
+		(*&lista_id)[i] = z.ID
 	}
 }
-func zapisPlik() {
-
+func zapisPlik(nazwapliku string) {
+	zapis, err := json.Marshal(zadania)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile(nazwapliku,zapis,0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print("Zadania zapisane do pliku")
 }
 func main() {
 	var opcja int
@@ -70,12 +77,14 @@ func main() {
 	var UsunID int
 	var WykonajID int
 	var Sciezka string
+	var nazwa string
 	for {
 		fmt.Print("-----------------------------\n")
 		fmt.Print("Wybierz opcję\n1.Wyświetl zadania.\n2.Dodaj zadanie.\n3.Usuń zadanie.\n4.Oznacz jako wykonane.\n5.Wczytaj Zadania z Pliku.\n6.Zapisz zadania do Pliku\n7.Opuść program\nNapisz numer opcji:  ")
 		fmt.Scan(&opcja)
 		switch opcja {
 		case 1:
+			fmt.Print(lista_id)
 			fmt.Print("-----------------------------\n")
 			if len(zadania) == 0{
 				fmt.Print("Lista zadań jest pusta, dodaj zadania.\n")
@@ -86,24 +95,39 @@ func main() {
 			fmt.Print("-----------------------------\n")
 			fmt.Print("Podaj nazwę zadania: ")
 			fmt.Scan(&UtworzID)
-			DodajZadanie(UtworzID)
+			if UtworzID == ""{
+				fmt.Print("Nazwa nie może być pusta.")
+			}else{
+				DodajZadanie(UtworzID)
+			}
 		case 3:
 			fmt.Print("-----------------------------\n")
 			fmt.Print("Napisz numer zadania które chcesz usunąć: ")
 			fmt.Scan(&UsunID)
-			UsunZadanie(UsunID)
+			if len(zadania) == 0{
+				fmt.Print("Lista zadań jest pusta, dodaj zadania.\n")
+			}else{
+				UsunZadanie(UsunID)
+			}
 		case 4:
 			fmt.Print("-----------------------------\n")
 			fmt.Print("Napisz numer zadania które wykonałeś: ")
 			fmt.Scan(&WykonajID)
-			oznacz(WykonajID)
+			if len(zadania) == 0{
+				fmt.Print("Lista zadań jest pusta, dodaj zadania.\n")
+			}else{
+				oznacz(WykonajID)
+			}
 		case 5:
 			fmt.Print("-----------------------------\n")
 			fmt.Print("Podaj ścieżke do pliku: ")
 			fmt.Scan(&Sciezka)
-			odczytPlik(Sciezka,zadania,lista_id)
+			odczytPlik(Sciezka)
 		case 6:
-
+			fmt.Print("-----------------------------\n")
+			fmt.Print("Podaj nazwę pliku: ")
+			fmt.Scan(&nazwa)
+			zapisPlik(nazwa)
 		case 7:
 			os.Exit(0)
 		default:
